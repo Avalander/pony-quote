1a56e80ae6cca86d7f3d5987fa54b9da81602981
Avalander <darth.quesorico@gmail.com>
2018-07-06 09:37:40 +0200

    Change separator to pipe character instead of semicolon.

--- pony-quote.sh
+++ pony-quote.sh
@@ -1,10 +1,10 @@
 QUOTES=(
-	'Twilight Sparkle;All the ponies in this town are CRAZY!'
-	"Twilight Sparkle;Huh? I'm pancake...I mean awake!"
-	'Pinkie Pie;Eternal chaos comes with chocolate rain, you guys. Chocolate rain!'
-	'Pinkie Pie;Too old for free candy? Never!'
+	'Twilight Sparkle|All the ponies in this town are CRAZY!'
+	"Twilight Sparkle|Huh? I'm pancake...I mean awake!"
+	'Pinkie Pie|Eternal chaos comes with chocolate rain, you guys. Chocolate rain!'
+	'Pinkie Pie|Too old for free candy? Never!'
 )
 
 selected=${QUOTES[$RANDOM % ${#QUOTES[@]}]}
-IFS=';' read -ra quote <<< "$selected"
+IFS='|' read -ra quote <<< "$selected"
 echo "${quote[0]} says ${quote[1]}"
